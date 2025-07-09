27b80fda8d70f3559c4dec5c73f24c841e54adc9
Sanadamaru <48264954+Sanadamaru01@users.noreply.github.com>
2025-07-09 19:30:39 +0900

    Delete js directory

--- js/configLoader.js
+++
@@ -1,5 +0,0 @@
-export async function loadConfig(path) {
-  const response = await fetch(path);
-  if (!response.ok) throw new Error(`Failed to load config: ${path}`);
-  return await response.json();
-}
