9cb98c1c1a3e71bd9751c70e9ba3c9e04b2d603b
Sanadamaru <48264954+Sanadamaru01@users.noreply.github.com>
2025-12-01 10:52:34 +0900

    Update firebaseFirestore.js

--- js/UploadTool/firebaseFirestore.js
+++ js/UploadTool/firebaseFirestore.js
@@ -1,76 +1,8 @@
 // firebaseFirestore.js
-import { app } from "./firebaseApp.js";
-import {
-  getFirestore,
-  collection,
-  doc,
-  setDoc,
-  addDoc,
-  updateDoc,
-  deleteDoc,
-  getDocs,
-  getDoc,
-  serverTimestamp
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { collection, doc, setDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { db } from "./firebaseApp.js";
 
-const db = getFirestore(app);
-
-/** 画像メタデータの保存 */
-export async function saveImageMetadata(roomId, imageId, { file, title, caption, author }) {
-  const docRef = doc(db, "rooms", roomId, "images", imageId);
-
-  await setDoc(docRef, {
-    file,
-    title,
-    caption,
-    author,
-    createdAt: serverTimestamp(),
-    updatedAt: serverTimestamp(),
-  });
-}
-
-/** 画像メタデータの取得 */
-export async function getAllImages(roomId) {
-  const colRef = collection(db, "rooms", roomId, "images");
-  const snap = await getDocs(colRef);
-
-  const result = [];
-  snap.forEach((d) => {
-    result.push({ id: d.id, ...d.data() });
-  });
-  return result;
-}
-
-/** メタデータ更新 */
-export async function updateImageMetadata(roomId, imageId, data) {
-  const docRef = doc(db, "rooms", roomId, "images", imageId);
-  await updateDoc(docRef, {
-    ...data,
-    updatedAt: serverTimestamp(),
-  });
-}
-
-/** 画像メタデータ削除 */
-export async function deleteImageMetadata(roomId, imageId) {
-  const docRef = doc(db, "rooms", roomId, "images", imageId);
-  await deleteDoc(docRef);
-}
-
-/** ルーム一覧取得 */
-export async function getRooms() {
-  const colRef = collection(db, "rooms");
-  const snap = await getDocs(colRef);
-
-  const rooms = [];
-  snap.forEach((d) => rooms.push({ id: d.id, ...d.data() }));
-  return rooms;
-}
-
-/** ルームタイトル更新 */
-export async function updateRoom(roomId, data) {
-  const docRef = doc(db, "rooms", roomId);
-  await updateDoc(docRef, {
-    ...data,
-    updatedAt: serverTimestamp(),
-  });
+export async function saveImageMetadata(roomId, imageId, data) {
+    const docRef = doc(db, `rooms/${roomId}/images/${imageId}`);
+    await setDoc(docRef, { ...data, createdAt: serverTimestamp(), updatedAt: serverTimestamp() });
 }
