8634ca8cf31c4be66a207018798ad28b3cd4564d
Sanadamaru <48264954+Sanadamaru01@users.noreply.github.com>
2025-12-01 19:16:14 +0900

    Create firebaseInit.js

---
+++ js/UploadTool/firebaseInit.js
@@ -0,0 +1,11 @@
+// firebaseInit.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY", // <- 自分の値に置き換えてください
+  authDomain: "gallery-us-ebe6e.firebaseapp.com",
+  projectId: "gallery-us-ebe6e",
+  storageBucket: "gallery-us-ebe6e.appspot.com",
+};
+
+export const app = initializeApp(firebaseConfig);
