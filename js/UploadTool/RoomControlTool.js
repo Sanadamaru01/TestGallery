1e1424b0b0e94db293a7f8daf7adb31eeb8f74e8
Sanadamaru <48264954+Sanadamaru01@users.noreply.github.com>
2026-01-08 15:49:37 +0900

    Update RoomControlTool.js

--- js/UploadTool/RoomControlTool.js
+++ js/UploadTool/RoomControlTool.js
@@ -131,8 +131,8 @@ async function loadRoomList() {
     const img = document.createElement("img");
     img.className = "thumb";
 
-    //const thumbRef = ref(storage, `roomThumbnails/${roomId}.webp`);
-    const thumbRef = ref(storage,`gs://gallery-us-ebe6e.appspot.com/roomThumbnails/${roomId}.webp`);
+    const thumbRef = ref(storage, `roomThumbnails/${roomId}.webp`);
+    //const thumbRef = ref(storage,`gs://gallery-us-ebe6e.appspot.com/roomThumbnails/${roomId}.webp`);
 
     getDownloadURL(thumbRef)
       .then(url => img.src = url)
@@ -217,8 +217,8 @@ resetRoomBtn.addEventListener("click", async () => {
     await deleteDoc(doc(db, `rooms/${roomId}/images`, docSnap.id));
   }
 
-  //const roomStorageDir = ref(storage, `rooms/${roomId}`);
-  const roomStorageDir = ref(storage, `gs://gallery-us-ebe6e.appspot.com/rooms/${roomId}`);
+  const roomStorageDir = ref(storage, `rooms/${roomId}`);
+  //const roomStorageDir = ref(storage, `gs://gallery-us-ebe6e.appspot.com/rooms/${roomId}`);
   try {
     const list = await listAll(roomStorageDir);
     for (const fileRef of list.items) {
@@ -226,8 +226,8 @@ resetRoomBtn.addEventListener("click", async () => {
     }
   } catch {}
 
-  //const thumbRef = ref(storage, `roomThumbnails/${roomId}.webp`);
-  const thumbRef = ref(storage, `gs://gallery-us-ebe6e.appspot.com/roomThumbnails/${roomId}.webp`);
+  const thumbRef = ref(storage, `roomThumbnails/${roomId}.webp`);
+  //const thumbRef = ref(storage, `gs://gallery-us-ebe6e.appspot.com/roomThumbnails/${roomId}.webp`);
   deleteObject(thumbRef).catch(() => {});
 
   await updateDoc(doc(db, "rooms", roomId), {
